2649f87ec2054efb697b43c3caa1c1d3d95210c7
Glitch (mealplanner) <none>
2018-05-10 09:15:31 +0200

    first commit

--- server.js
+++ server.js
@@ -22,33 +22,33 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// init sqlite db
-var fs = require('fs');
-var dbFile = './.data/sqlite.db';
-var exists = fs.existsSync(dbFile);
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(dbFile);
-
-// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
-db.serialize(function(){
-  if (!exists) {
-    db.run('CREATE TABLE Dreams (dream TEXT)');
-    console.log('New table Dreams created!');
-    
-    // insert default dreams
-    db.serialize(function() {
-      db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
-    });
-  }
-  else {
-    console.log('Database "Dreams" ready to go!');
-    db.each('SELECT * from Dreams', function(err, row) {
-      if ( row ) {
-        console.log('record:', row);
-      }
-    });
-  }
-});
+// // init sqlite db
+// var fs = require('fs');
+// var dbFile = './.data/sqlite.db';
+// var exists = fs.existsSync(dbFile);
+// var sqlite3 = require('sqlite3').verbose();
+// var db = new sqlite3.Database(dbFile);
+//
+// // if ./.data/sqlite.db does not exist, create it, otherwise print records to console
+// db.serialize(function(){
+//   if (!exists) {
+//     db.run('CREATE TABLE Dreams (dream TEXT)');
+//     console.log('New table Dreams created!');
+//
+//     // insert default dreams
+//     db.serialize(function() {
+//       db.run('INSERT INTO Dreams (dream) VALUES ("Find and count some sheep"), ("Climb a really tall mountain"), ("Wash the dishes")');
+//     });
+//   }
+//   else {
+//     console.log('Database "Dreams" ready to go!');
+//     db.each('SELECT * from Dreams', function(err, row) {
+//       if ( row ) {
+//         console.log('record:', row);
+//       }
+//     });
+//   }
+// });
 
 
 //Routen verteilen
@@ -63,11 +63,11 @@ app.use('/planer',planerRouter);
 // endpoint to get all the dreams in the database
 // currently this is the only endpoint, ie. adding dreams won't update the database
 // read the sqlite3 module docs and try to add your own! https://www.npmjs.com/package/sqlite3
-app.get('/getDreams', function(request, response) {
-  db.all('SELECT * from Dreams', function(err, rows) {
-    response.send(JSON.stringify(rows));
-  });
-});
+// app.get('/getDreams', function(request, response) {
+//   db.all('SELECT * from Dreams', function(err, rows) {
+//     response.send(JSON.stringify(rows));
+//   });
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
